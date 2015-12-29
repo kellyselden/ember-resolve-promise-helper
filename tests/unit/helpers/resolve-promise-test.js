796c993720663be79eb01e6e23844eac8e1e7a51
Kelly Selden <kellyselden@gmail.com>
2015-12-28 23:04:26 -0500

    adding resolve-promise helper

---
+++ tests/unit/helpers/resolve-promise-test.js
@@ -0,0 +1,10 @@
+import { resolvePromise } from '../../../helpers/resolve-promise';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | resolve promise');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = resolvePromise(42);
+  assert.ok(result);
+});
